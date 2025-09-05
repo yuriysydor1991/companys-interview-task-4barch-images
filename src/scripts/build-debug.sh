33ad0a1fe7f48bc6e1c07ef9e22af3e37fa33bab
Yuriy Sydor <yuriysydor1991@gmail.com>
2025-09-05 17:33:49 +0300

    Enabling sanitizers in the debug quick build script

--- src/scripts/build-debug.sh
+++ src/scripts/build-debug.sh
@@ -4,9 +4,11 @@ clear
 
 rm -fr build 
 
+# for thread sanitizers - ENABLE_SANITIZERS_THREADS
 if ! cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DENABLE_CLANGFORMAT=ON \
   -DENABLE_CPPCHECK=ON -DENABLE_COMPONENT_TESTS=ON \
-  -DENABLE_UNIT_TESTS=ON -DMAX_LOG_LEVEL=6
+  -DENABLE_UNIT_TESTS=ON -DMAX_LOG_LEVEL=6 \
+  -DENABLE_SANITIZERS=ON
 then
   echo "####### fail to configure! #####"
   exit 1
